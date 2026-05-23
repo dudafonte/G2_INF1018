2170567d561bd1e77946eece6768c890a845f683
Maria Eduarda <m.eduardafonte24@gmail.com>
2026-05-23 23:40:47 +0000

    inicio

---
+++ cria_func.c
@@ -0,0 +1,9 @@
+/* Lívia Ramos Vallim            2300000 3WA */
+/* Maria Eduarda Fonte de Macedo 2320450 3WA */
+
+#include "cria_func.h"
+
+void cria_func(void* f, DescParam params[], int n, unsigned char codigo[]) 
+{
+    /*alalala*/
+}
